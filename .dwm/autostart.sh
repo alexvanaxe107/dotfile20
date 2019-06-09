8741d8fcdace2f6c48594f43d26289ce10164f49
Alexvanaxe <alexvanaxe@gmail.com>
2019-06-09 16:53:28 -0300

    dwm

---
+++ .dwm/autostart.sh
@@ -0,0 +1,8 @@
+dunst &
+nitrogen --restore &
+$HOME/.config/i3/monitor/saver.sh &
+killall conky
+/home/alexvanaxe/.config/i3/conky/conky.sh &
+compton -b &
+killall dwmstatusbar
+~/.dwm/dwmstatusbar &
