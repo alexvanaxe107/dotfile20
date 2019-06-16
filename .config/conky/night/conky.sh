4a81cc88b37d0aefcae147f944e1422bb4e7e474
Alexvanaxe <alexvanaxe@gmail.com>
2019-06-16 19:11:51 -0300

    Conky adjustments

--- .config/conky/night/conky.sh
+++ .config/conky/night/conky.sh
@@ -1,3 +1,4 @@
 conky -d --config ~/.config/conky/night/cpu.conf
+conky -d --config ~/.config/conky/night/fortune.conf
 conky -d --config ~/.config/conky/night/clock.conf
 conky -d --config ~/.config/conky/night/process.conf
