aa69c4a0523f38b7164f0da2979689dfefe9f7c5
Alexvanaxe <alexvanaxe@gmail.com>
2019-06-16 16:35:52 -0300

    New conky config

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -2,7 +2,8 @@ dunst &
 nitrogen --restore &
 $HOME/.config/i3/monitor/saver.sh &
 killall conky
-/home/alexvanaxe/.config/i3/conky/conky.sh &
+# /home/alexvanaxe/.config/i3/conky/conky.sh &
+/home/alexvanaxe/.config/conky/day/conky.sh
 compton -b &
 killall dwmstatusbar
 ~/.dwm/dwmstatusbar &
