4685a3943de432729fe58b20aca8a2fd498a6d6f
Alexvanaxe <alexvanaxe@gmail.com>
2019-06-20 15:36:14 -0300

    Improving recorder

--- bin/screeneaudio.sh
+++ bin/screeneaudio.sh
@@ -1,6 +1,6 @@
 
 if [[ $1 == "play" ]]; then
-    ffmpeg -video_size 1366x768 -framerate 25 -f x11grab -i :0 -f pulse -ac 2 -i default ~/Documents/output.mkv
+    ffmpeg -video_size 1366x768 -thread_queue_size 512 -framerate 25 -f x11grab -i :0 -f pulse -thread_queue_size 512 -itsoffset 0.320 -ac 2 -i default ~/Documents/output.mkv
 fi
 
 if [[ $1 == "stop" ]]; then
