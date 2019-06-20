e1637b64471759668c860429f4ab5d4226d2d58a
Alexvanaxe <alexvanaxe@gmail.com>
2019-06-20 19:40:18 -0300

    Changed a lot

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -3,7 +3,7 @@ nitrogen --restore &
 $HOME/.config/i3/monitor/saver.sh &
 killall conky
 # /home/alexvanaxe/.config/i3/conky/conky.sh &
-/home/alexvanaxe/.config/conky/day/conky.sh
+/home/alexvanaxe/.config/conky/night/conky.sh
 compton -b &
 killall dwmstatusbar
 ~/.dwm/dwmstatusbar &
--- bin/pomodoro-client.sh
+++ bin/pomodoro-client.sh
@@ -7,12 +7,12 @@ then
 fi
 
 source $HOME/.pyenv/versions/spotify/bin/activate
-chosen=$(printf "⏸\\n▶\\n🕑\\n⏹\\n▶▶\\n↻" | dmenu "$@" -i -p "$($HOME/.config/i3/scripts/pomodoro-client.py status) ")
+chosen=$(printf "⏸\\n▶\\n\\n⏹\\n▶▶\\n↻" | dmenu "$@" -i -p "$($HOME/.config/i3/scripts/pomodoro-client.py status) ")
 
 
 case "$chosen" in
 	"⏸") python $HOME/.config/i3/scripts/pomodoro-client.py pause;;
-	"🕑") python $HOME/.config/i3/scripts/pomodoro-client.py start;;
+	"") python $HOME/.config/i3/scripts/pomodoro-client.py start;;
 	"▶") python $HOME/.config/i3/scripts/pomodoro-client.py resume;;
 	"⏹") python $HOME/.config/i3/scripts/pomodoro-client.py stop;;
 	"▶▶") python $HOME/.config/i3/scripts/pomodoro-client.py skip;;
