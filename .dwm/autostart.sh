d3bd7b85491a1ae44fb4b6503b10c008c3177f04
Alexvanaxe <alexvanaxe@gmail.com>
2019-06-30 15:12:01 -0300

    Improvement in autostart

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -2,9 +2,10 @@ dunst &
 nitrogen --restore &
 $HOME/.config/i3/monitor/saver.sh &
 killall conky
-# /home/alexvanaxe/.config/i3/conky/conky.sh &
-/home/alexvanaxe/.config/conky/night/conky.sh
+/home/alexvanaxe/.config/i3/conky/conky.sh &
+#/home/alexvanaxe/.config/conky/night/conky.sh
 compton -b &
+killall stalonetray &
 stalonetray &
 killall dwmstatusbar
 ~/.dwm/dwmstatusbar &
