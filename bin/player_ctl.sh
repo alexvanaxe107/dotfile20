d2b95c76e60eb38a979b06ffa050ba4a93841d87
John Smith <John.Smith@example.com>
2019-09-18 18:14:25 -0300

    Updated player_ctl dmenu script

--- bin/player_ctl.sh
+++ bin/player_ctl.sh
@@ -1,10 +1,33 @@
 #!/bin/sh
 
-chosen=$(printf "pause ⏸\\nplay ▶\\nforward ▶▶\\nback ◀◀" | dmenu "$@" -i -p "$(playerctl metadata artist) - $(playerctl metadata title)")
 
-case "$chosen" in
-	"pause ⏸") playerctl pause;;
-	"play ▶") playerctl play;;
-	"forward ▶▶") playerctl next;;
-	"back ◀◀") playerctl previous;;
-esac
+players=($(playerctl -a metadata 2> /dev/null | awk '{print $1}' | uniq))
+
+players_len=${#players[@]}
+
+if [ $players_len -gt 1 ]; then
+    chosen_p=$(printf "%s\n ${players[@]}" | dmenu "$@" -i -p "Select the player:")
+
+    if [ ! -z $chosen_p ]; then
+        chosen=$(printf "pause ⏸\\nplay ▶\\nforward ▶▶\\nback ◀◀" | dmenu "$@" -i -p "$(playerctl -p $chosen_p metadata artist) - $(playerctl -p $chosen_p metadata title)")
+
+        case "$chosen" in
+            "pause ⏸") playerctl -p $chosen_p pause;;
+            "play ▶") playerctl -p $chosen_p play;;
+            "forward ▶▶") playerctl -p $chosen_p next;;
+            "back ◀◀") playerctl -p $chosen_p previous;;
+        esac
+    fi
+
+
+else
+    chosen=$(printf "pause ⏸\\nplay ▶\\nforward ▶▶\\nback ◀◀" | dmenu "$@" -i -p "$(playerctl metadata artist) - $(playerctl metadata title)")
+
+    case "$chosen" in
+        "pause ⏸") playerctl pause;;
+        "play ▶") playerctl play;;
+        "forward ▶▶") playerctl next;;
+        "back ◀◀") playerctl previous;;
+    esac
+fi
+
