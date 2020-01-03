14719e7768ea5a2af7d8dc5b9f6537246a7c4ba6
Alexvanaxe <alexvanaxe@gmail.com>
2020-01-02 22:01:00 -0300

    Updating bin and themes

--- bin/polybar_toggle.sh
+++ bin/polybar_toggle.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-PID=$(pgrep -a polybar | grep "$theme_name" | awk '{print $1}')
+PID=$(pgrep -a polybar | grep "default" | awk '{print $1}')
 
 if [ ! -z $PID ]; then
     killall polybar
     bspc config top_padding 0
 else
-    polybar $theme_name &
+    polybar default &
     sleep 1
     bspc config top_padding 16
 fi
