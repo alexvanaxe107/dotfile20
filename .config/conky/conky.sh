a198eef1c8e0f9bbb03fcd72fca4b5f6359ab14a
Alexvanaxe <alexvanaxe@gmail.com>
2020-02-02 22:10:24 -0300

    Toggle conky

--- .config/conky/conky.sh
+++ .config/conky/conky.sh
@@ -1,4 +1,13 @@
-conky -d --config ~/.config/conky/cpu.conf
-conky -d --config ~/.config/conky/fortune.conf
-conky -d --config ~/.config/conky/clock.conf
-conky -d --config ~/.config/conky/process.conf
+#!/bin/sh
+
+pid=$(pidof conky)
+if [ -z "$pid" ]
+then
+        conky -d --config ~/.config/conky/cpu.conf
+        conky -d --config ~/.config/conky/fortune.conf
+        conky -d --config ~/.config/conky/clock.conf
+        conky -d --config ~/.config/conky/process.conf
+    else
+        killall conky
+
+fi
