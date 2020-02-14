372af54bdb8d0e928923fa0037a9403e1f0b3617
Alexvanaxe <alexvanaxe@gmail.com>
2020-02-13 21:26:43 -0300

    Update config file

--- bin/configshortcut.sh
+++ bin/configshortcut.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-file=$(echo -e "sxhkd\nbspwm\nradio\nconfig" | dmenu "$@" -p "Select file to edit")
+file=$(echo -e "sxhkd\nbspwm\nradio\nconfig\nzshrc" | dmenu "$@" -p "Select file to edit")
 
 case $file in
     "sxhkd") alacritty -e $EDITOR $HOME/.config/sxhkd/sxhkdrc;;
     "bspwm") alacritty -e $EDITOR $HOME/.config/bspwm/bspwmrc;;
     "radio") alacritty -e $EDITOR $HOME/.config/play_radio/config /home/alexvanaxe/.config/play_radio/config.cast;;
-    "config") alacritty -e $EDITOR $HOME/.config;;
+    "config") alacritty -e $EDITOR $HOME/bin/configshortcut.sh;;
+    "zshrc") alacritty -e $EDITOR $HOME/.zshrc;;
 esac
 
