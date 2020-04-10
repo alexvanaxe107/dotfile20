0c747e6ba3f506357cc326eb5233a6b32a272eb3
Alexvanaxe <alexvanaxe@gmail.com>
2020-04-09 22:16:22 -0300

    Play chosing the quality

--- bin/play_radio.sh
+++ bin/play_radio.sh
@@ -1,46 +1,80 @@
 #!/bin/sh
-play_local(){
+
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+
+function play_local(){
+    notify-send -u normal  "Playing..." "Playing your radio now. Enjoy! =)"
+
     if [[ $url == *"pls"* ]]; then
-        mpv -playlist $url&
+        mpv -playlist $url
     else
-        mpv $url&
+        mpv $url
     fi
+    notify-send -u normal  "Finished" "Media stoped. Bye."
+    exit
 }
 
-play_cast(){
+function play_cast(){
     castnow --quiet $url&
 }
 
-stop_all(){
-    killall mpv&
+function stop_all(){
+    killall mpv
     #castnow --quiet --command s --exit&
 }
 
-play_clipboard(){
+function play_clipboard(){
+    notify-send -u normal  "Trying to play..." "Playing your media now the best way we can. Enjoy."
+
     result=$(xclip -o)
     mpv "$result"
+
+    notify-send -u normal  "Done" "Hopefully your media was played =/"
     exit
 }
 
-play_clipboard_low_quality(){
+function play_clipboard_quality(){
     result=$(xclip -o)
-    mpv "$result" --ytdl-format=18
+    local option="$(youtube-dl --list-formats "${result}" | sed -n '6,$p')"
+
+    local chosen_p=$(basename -a "${option}" | dmenu  -l 10 -i -p "Select the player:")
+
+    local choosen_quality=$(awk '{print $1}' <<< ${chosen_p})
+
+    if [[ -z "${choosen_quality}" ]]; then
+        notify-send -u normal  "Your choice" "None choice was made. Exiting"
+        exit 0
+    fi
+
+    notify-send -u normal  "Trying to play" "The media will be played soon... wait a little and enjoy."
+
+    mpv "$result" --ytdl-format=${choosen_quality}
+
+    notify-send -u normal  "Done" "Hopefully your media was played =/"
     exit
 }
 
-play_clipboard_audio(){
+function play_clipboard_audio(){
+    notify-send -u normal  "Trying to play..." "Playing your media as audio now the best way we can. Enjoy."
     result=$(xclip -o)
     mpv "$result" --no-video
+    notify-send -u normal  "Done" "Hopefully your media was played =/"
     exit
 }
 
-chosen_mode=$(printf "Local\\nCast\\nStop\\nClipboard\\nClipboard Audio\\nClipboard Low quality" | dmenu "$@" -i -p "Where to play?")
+chosen_mode=$(printf "Local\\nCast\\nStop\\nClipboard\\nClipboard Audio\\nClipboard quality" | dmenu "$@" -i -p "Where to play?")
 
 case "$chosen_mode" in
     "Local") radio_file="$HOME/.config/play_radio/config";;
     "Cast") radio_file="$HOME/.config/play_radio/config.cast";;
     "Clipboard") play_clipboard;;
-    "Clipboard Low quality") play_clipboard_low_quality;;
+    "Clipboard quality") play_clipboard_quality;;
     "Clipboard Audio") play_clipboard_audio;;
     "Stop") $(stop_all);;
     *) exit;;
--- bin/teste.sh
+++ bin/teste.sh
@@ -1,19 +1,19 @@
 #! /bin/sh
+#! /bin/sh
 
 set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
 
-function teste() {
-    local cur_wallpaper=$(cat $XDG_CONFIG_HOME/nitrogen/bg-saved.cfg | grep file | awk 'BEGIN{FS="="} NR==2 {print $2}')
-    local colors_wallpaper=($(convert "${cur_wallpaper}" -format %c -depth 10  histogram:info:- | grep -o "#......" | cut -d "#" -f 2))
+echo "Iniciando teste"
 
-    if [[ ${#colors_wallpaper[@]} -lt 140 ]]; then
-        echo ${#colors_wallpaper[@]}
-    else
-        echo "TEM MAIS"
-    fi
-    echo ${cur_wallpaper}
-    echo ${colors_wallpaper[100]}
-}
+option="$(youtube-dl --list-formats "https://www.youtube.com/watch?v=det3U5eqGb8" | sed -n '6,$p')"
 
-teste123=$(teste)
-echo ${teste123}
+chosen_p=$(basename -a "${OPTIONS}" | dmenu  -l 10 -i -p "Select the player:")
+
+awk '{print $1}' <<< ${chosen_p}
+
