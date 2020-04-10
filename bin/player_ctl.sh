962d1c5f7504bb3603ab6339e7b3d26cdfdb2ace
Alexvanaxe <alexvanaxe@gmail.com>
2020-04-10 14:05:22 -0300

    Controling the time

--- bin/player_ctl.sh
+++ bin/player_ctl.sh
@@ -4,31 +4,41 @@ players=($(playerctl -a metadata 2> /dev/null | awk '!seen[$1] {print $1} {++see
 
 players_len=${#players[@]}
 
+function go_to_position() {
+    time=$1
+   
+    goto=$(echo "${time}" | bc)
+    playerctl position "${goto}"
+}
+
+
 if [ $players_len -gt 1 ]; then
     chosen_p=$(basename -a ${players[@]} | dmenu "$@" -i -p "Select the player:")
 
     if [ ! -z $chosen_p ]; then
-        chosen=$(printf "pause ⏸\\nplay ▶\\nforward ▶▶\\nback ◀◀" | dmenu "$@" -i -p "$(playerctl -p $chosen_p metadata artist) - $(playerctl -p $chosen_p metadata title)")
+        position=$(echo "$(playerctl position) / 60" | bc)
+        chosen=$(printf "pause ⏸\\nplay ▶\\nforward ▶▶\\nback ◀◀" | dmenu "$@" -i -p "${position}Min:$(playerctl -p $chosen_p metadata artist) - $(playerctl -p $chosen_p metadata title)}")
 
         case "$chosen" in
             "pause ⏸") playerctl -p $chosen_p pause;;
             "play ▶") playerctl -p $chosen_p play;;
             "forward ▶▶") playerctl -p $chosen_p next;;
             "back ◀◀") playerctl -p $chosen_p previous;;
-            *) playerctl position "${chosen}";;
+            *) go_to_position ${chosen};;
         esac
     fi
 
 
 else
-    chosen=$(printf "pause ⏸\\nplay ▶\\nforward ▶▶\\nback ◀◀" | dmenu "$@" -i -p "$(playerctl metadata artist) - $(playerctl metadata title)")
+    position=$(echo "$(playerctl position) / 60" | bc)
+    chosen=$(printf "pause ⏸\\nplay ▶\\nforward ▶▶\\nback ◀◀" | dmenu "$@" -i -p "${position}Min: $(playerctl metadata artist) - $(playerctl metadata title)")
 
     case "$chosen" in
         "pause ⏸") playerctl pause;;
         "play ▶") playerctl play;;
         "forward ▶▶") playerctl next;;
         "back ◀◀") playerctl previous;;
-        *) playerctl position "${chosen}";;
+        *) go_to_position ${chosen};;
     esac
 fi
 
--- bin/teste.sh
+++ bin/teste.sh
@@ -9,11 +9,4 @@ set -o nounset
 # Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
 set -o pipefail
 
-echo "Iniciando teste"
-
-option="$(youtube-dl --list-formats "https://www.youtube.com/watch?v=det3U5eqGb8" | sed -n '6,$p')"
-
-chosen_p=$(basename -a "${OPTIONS}" | dmenu  -l 10 -i -p "Select the player:")
-
-awk '{print $1}' <<< ${chosen_p}
-
+echo ${teste}
