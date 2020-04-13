9e0346ffb123c2da890c29826bd34676c7ba4cf8
Alexvanaxe <alexvanaxe@gmail.com>
2020-04-13 15:20:05 -0300

    Adding config to vimb

--- bin/configshortcut.sh
+++ bin/configshortcut.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-file=$(echo -e "sxhkd\nbspwm\nradio\nconfig\nzshrc" | dmenu "$@" -p "Select file to edit")
+file=$(echo -e "sxhkd\nbspwm\nradio\nconfig\nzshrc\nvimb" | dmenu "$@" -p "Select file to edit")
 
 case $file in
     "sxhkd") alacritty -e $EDITOR $HOME/.config/sxhkd/sxhkdrc;;
@@ -8,5 +8,6 @@ case $file in
     "radio") alacritty -e $EDITOR $HOME/.config/play_radio/config /home/alexvanaxe/.config/play_radio/config.cast;;
     "config") alacritty -e $EDITOR $HOME/bin/configshortcut.sh;;
     "zshrc") alacritty -e $EDITOR $HOME/.zshrc;;
+    "vimb") alacritty -e $EDITOR $HOME/.config/vimb/config;;
 esac
 
