2479d4c92c1e126921ec88f2d93eafb041bdf8c4
Alexvanaxe <alexvanaxe@gmail.com>
2020-04-23 16:35:07 -0300

    Configured fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,18 @@
+
+############     ALIASES     #############
+alias pacupg='sudo pacman -Syu'		# Synchronize with repositories and then upgrade packages that are out of date on the local system.
+alias pacre='sudo pacman -R'		# Remove the specified package(s), retaining its configuration(s) and required dependencies
+
+alias fdir='fdir.sh'
+
+############     SETS     #############
+set -gx PATH "$HOME/bin" "$PYENV_ROOT/bin" $PATH
+set -x EDITOR nvim
+set -x SUDO_ASKPASS "/usr/bin/lxqt-openssh-askpass"
+set -x FZF_DEFAULT_COMMAND 'fd -I --type f'
+
+############     SOURCES     #############
+source $HOME/.fdirrc
+
+###########     INIT PYENV     ###########
+pyenv init - | source
