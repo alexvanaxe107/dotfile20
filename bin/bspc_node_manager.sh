03bb6d7e3c10aff24d3a297e5e8b60576bc34cb4
Alexvanaxe <alexvanaxe@gmail.com>
2020-05-01 14:33:16 -0300

    Added shortcut to windows

---
+++ bin/bspc_node_manager.sh
@@ -0,0 +1,37 @@
+#! /bin/dash
+
+retrieve_index() {
+	selected=$(wmctrl -l | awk '{$1=$2=$3="";print NR $0}' | dmenu -l 10)
+
+	index=$(echo $selected | awk '{print $1}')
+
+	echo ${index}
+}
+
+retrieve_program() {
+	index=$1
+	
+	code=$(wmctrl -l | awk -v IND=${index} 'NR==IND {print $1}')
+	echo ${code}
+}
+
+focus_program() {
+	node=$1
+	bspc node $1 --focus
+}
+
+bring_program() {
+	node=$1
+	bspc node $1 --to-desktop focused
+}
+
+action=$1
+
+indice=$(retrieve_index)
+code=$(retrieve_program $indice)
+
+case "${action}" in
+	"-b") bring_program ${code};;
+	*) focus_program ${code};;
+esac
+
