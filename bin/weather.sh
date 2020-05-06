d6988563cb7fba91b930b4981052fce1e1103c28
Alexvanaxe <alexvanaxe@gmail.com>
2020-05-06 19:42:28 -0300

    Improving weather

--- bin/weather.sh
+++ bin/weather.sh
@@ -2,7 +2,11 @@
 
 weather_show(){
     temp=$(curl -Ss 'https://wttr.in?format=%c+%t+%m' 2> /dev/null)
-	echo "$temp"
+    retorna=$(echo $temp | grep -i unknown)
+    if [ -z "$retorna" ]
+    then
+        echo "$temp"
+    fi
 }
 
 weather_show
