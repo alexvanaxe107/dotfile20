d458c70582cda623659ca2a03ceac4be290dc3db
Alexvanaxe <alexvanaxe@gmail.com>
2020-05-05 22:52:28 -0300

    Protection removed from weather

--- bin/weather.sh
+++ bin/weather.sh
@@ -2,13 +2,7 @@
 
 weather_show(){
     temp=$(curl -Ss 'https://wttr.in?format=%c+%t+%m' 2> /dev/null)
-    regex='[\+\-][0-9].*C'
-    temp_number=$(echo $number | tr -dc '[:alnum:]\n\r' | tr '[:upper:]' '[:lower:]')
-    if  [ $(echo  "$temp_number" | grep -E $regex) ]; then
-        echo $temp > ~/.dwm/weather.cache
-    fi
-	foo="`cat ~/.dwm/weather.cache`"
-	echo "$foo"
+	echo "$temp"
 }
 
 weather_show
