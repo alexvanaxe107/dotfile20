056974e64b93ad057dad73af53e050df914045af
Alexvanaxe <alexvanaxe@gmail.com>
2020-05-24 17:22:50 -0300

    Little cosmetic changes

--- bin/font_select.sh
+++ bin/font_select.sh
@@ -8,7 +8,7 @@ set -o nounset
 # Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
 #set -o pipefail
 
-CHOSEN=$(printf "Day Original\\nNight Original\\nImpressive\\nElegant\\nElite\\nWestern\\n80s\\nSpaceShip\\nOld Terminal\\nProgramming\\nSoft\\nBook\\nCursive" | dmenu -i -p "Change the font: ")
+CHOSEN=$(printf "Day Original\\nNight Original\\nWasteland\\nElegant\\nElegant2\\nWar\\nStocks\\nWestern\\n80s\\nNeon\\nCyberpunk\\nOld Terminal\\nProgramming\\nSoft\\nBook\\nCursive\\nFuturistic" | dmenu -i -p "Change the font: ")
 
 if [ -z "${CHOSEN}" ]; then
     exit
@@ -36,11 +36,10 @@ font() {
     update=$(printf "Yes\nNo" | dmenu -i -p "Update Font?")
 
     if [ "${update}" = "Yes" ]; then
-        cp ${HOME}/.config/alacritty/alacritty.yml ${HOME}/.config/alacritty/alacritty_bkp_font.yml 
         sed -i "s/family:.*/family: ${font_name}/" ${HOME}/.config/alacritty/alacritty.yml
         sed -i "s/#size:.*/size: ${size3}/" ${HOME}/.config/alacritty/alacritty.yml
     else
-        cp ${HOME}/.config/alacritty/alacritty_bkp_font.yml ${HOME}/.config/alacritty/alacritty.yml 
+        cp ${HOME}/.config/alacritty/alacritty.${theme_name} ${HOME}/.config/alacritty/alacritty.yml 
     fi
     
 }
@@ -48,16 +47,20 @@ font() {
 case $CHOSEN in
     "Day Original") font "Erica Type" Bold 9 10 12 0;;
     "Night Original") font "Iceland" Regular 12 12 15 0;;
-    "Old Terminal") font "VT323" Regular 11 11 13 1;;
-    "Elite") font "Special Elite" Bold 10 10 12 2;;
+    "Old Terminal") font "VT323" Regular 11 11 14 1;;
+    "Stocks") font "Hack" Bold 9 10 12 1;;
     "Elegant") font "Unica One" Regular 9 10 12 1;;
-    "Impressive") font "Yeseva One" Bold 9 10 11 0;;
+    "Elegant2") font "NovaMono" Normal 11 11 14 1;;
+    "Neon") font "Segment14" Regular 9 9 14 1;;
+    "War") font "American Stencil" Regular 9 10 12 1;;
     "Programming") font "Fira Code" Bold 9 10 11 0;;
-    "Cursive") font "Z003" "Medium Italic" 12 11 12 1;;
+    "Cursive") font "Z003" "Medium Italic" 12 11 14 1;;
     "Soft") font "Fantasque Sans Mono" Regular 10 11 12 1;;
-    "80s") font "Audiowide" Regular 9 9 11 1;;
-    "SpaceShip") font "Orbitron" Regular 9 9 11 1;;
+    "80s") font "Press Start 2P" Regular 8 8 10 2;;
+    "Cyberpunk") font "Small Pixel7" Regular 11 11 14 1;;
     "Western") font "Graduate" Regular 9 9 11 1;;
-    "Book") font "Merchant Copy Double Size" Regular 8 9 11 2;;
+    "Book") font "CarbonType" Regular 8 9 11 1;;
+    "Wasteland") font "Beccaria" Bold 11 12 14 1;;
+    "Futuristic") font "Typo Draft Demo" Bold 11 12 14 1;;
     *) font "${CHOSEN}" Bold 9 10 11 0;;
 esac
