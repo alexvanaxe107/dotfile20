41f5e683638d22f7ce57685528fb5006002f0aad
Alexvanaxe <alexvanaxe@gmail.com>
2020-05-25 11:14:25 -0300

    Font updates

--- bin/font_select.sh
+++ bin/font_select.sh
@@ -8,7 +8,7 @@ set -o nounset
 # Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
 #set -o pipefail
 
-CHOSEN=$(printf "Day Original\\nNight Original\\nWasteland\\nElegant\\nElegant2\\nWar\\nStocks\\nWestern\\n80s\\nNeon\\nCyberpunk\\nOld Terminal\\nProgramming\\nSoft\\nBook\\nCursive\\nFuturistic" | dmenu -i -p "Change the font: ")
+CHOSEN=$(printf "Day Original\\nNight Original\\nWasteland\\nElegant\\nElegant2\\nWar\\nStocks\\nWestern\\n80s\\nNeon\\nCyberpunk\\nOld Terminal\\nProgramming\\nSoft\\nBook\\nCursive" | dmenu -i -p "Change the font: ")
 
 if [ -z "${CHOSEN}" ]; then
     exit
@@ -53,7 +53,7 @@ case $CHOSEN in
     "Elegant2") font "NovaMono" Normal 10 10 12 1;;
     "Neon") font "Segment14" Regular 9 9 14 1;;
     "War") font "American Stencil" Regular 9 10 12 1;;
-    "Programming") font "Fira Code" Bold 9 10 11 0;;
+    "Programming") font "Fira Code" Bold 9 10 11 1;;
     "Cursive") font "Z003" "Medium Italic" 12 11 14 1;;
     "Soft") font "Fantasque Sans Mono" Regular 10 11 12 1;;
     "80s") font "Press Start 2P" Regular 8 8 10 2;;
@@ -61,6 +61,5 @@ case $CHOSEN in
     "Western") font "Graduate" Regular 9 9 11 1;;
     "Book") font "Kingthings Trypewriter 2" Regular 10 11 14 1;;
     "Wasteland") font "Beccaria" Bold 11 12 14 1;;
-    "Futuristic") font "Typo Draft Demo" Bold 11 12 14 1;;
     *) font "${CHOSEN}" Bold 9 10 11 0;;
 esac
