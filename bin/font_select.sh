7acbbb7099e989d852909dc04f2a0d05fb935635
Alexvanaxe <alexvanaxe@gmail.com>
2020-05-31 22:09:08 -0300

    Fonts finished

--- bin/font_select.sh
+++ bin/font_select.sh
@@ -8,7 +8,7 @@ set -o nounset
 # Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
 #set -o pipefail
 
-CHOSEN=$(printf "Day Original\\nNight Original\\nWasteland\\nElegant\\nElegant2\\nStock\\nWar\\nMinimalist\\nModern\\nFuturistic\\nWestern\\n80s\\nNeon\\nCyberpunk\\nPixel\\nOld Terminal\\nProgramming\\nSoft\\nBook\\nCursive\\nCartoon\\nDelirium" | dmenu -i -l 22 -p "Change the font: ")
+CHOSEN=$(printf "Day Original\\nNight Original\\nWasteland\\nElegant\\nElegant2\\nStock\\nRock\\nWar\\nMinimalist\\nModern\\nFuturistic\\nWestern\\n80s\\nNeon\\nCyberpunk\\nPixel\\nOld Terminal\\nProgramming\\nSoft\\nBook\\nCursive\\nCartoon\\nDelirium\\nclear" | dmenu -i -l 30 -p "Change the font: ")
 
 if [ -z "${CHOSEN}" ]; then
     exit
@@ -62,23 +62,25 @@ case $CHOSEN in
     "Night Original") font "Iceland" Regular 12 12 15 0;;
     "Old Terminal") font "VT323" Regular 11 11 14 1;;
     "Minimalist") font "Nouveau IBM Stretch" Bold 9 10 14 0;;
+    "clear") font "TeX Gyre Cursor" Bold 9 10 14 0;;
     "Modern") font "CQ Mono" Bold 9 10 12 1;;
-    "Futuristic") font "minim" Normal 8 8 10 1;;
+    "Futuristic") font "Larabiefont Rg" Normal 8 8 10 1;;
     "Elegant") font "Unica One" Regular 9 10 12 1;;
     "Elegant2") font "NovaMono" Normal 9 9 12 1;;
     "Neon") font "Segment14" Regular 9 9 14 1;;
+    "Rock") font "Targa MS" Regular 10 10 13 1;;
     "War") font "American Stencil" Regular 9 10 12 1;;
     "Programming") font "Roboto Mono" Bold 10 10 11 1;;
     "Cursive") font "Z003" "Medium Italic" 12 11 14 1;;
     "Soft") font "mononoki" Regular 10 10 12 1;;
     "80s") font "Press Start 2P" Regular 7 7 10 2;;
-    "Cyberpunk") font "Terminus (TTF)" "Bold" 10 10 14 2;;
-    "Pixel") font "Small Pixel7" Regular 11 11 14 1;;
+    "Cyberpunk") font "Braciola MS" "Regular Rg" 10 10 12 1;;
+    "Pixel") font "Repetition Scrolling" Regular 10 10 13 1;;
     "Western") font "Graduate" Regular 9 9 11 1;;
     "Book") font "Kingthings Trypewriter 2" Regular 10 11 14 1;;
     "Stock") font "SVI Basic Manual" Bold 10 11 14 1;;
     "Wasteland") font "Beccaria" Bold 11 12 14 1;;
     "Cartoon") font "Pointfree" Bold 9 10 11 1;;
-    "Delirium") font "A.D. MONO" Bold 11 11 13 2;;
+    "Delirium") font "Plasmatic" Bold 10 10 13 1;;
     *) font "${CHOSEN}" Bold 9 10 11 0;;
 esac
