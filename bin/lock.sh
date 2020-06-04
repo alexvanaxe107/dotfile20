88cd01d7dc68e5e2e339579fcc93e3f4056f0ee7
Alexvanaxe <alexvanaxe@gmail.com>
2020-06-04 17:16:19 -0300

    Monitor lock save

--- bin/lock.sh
+++ bin/lock.sh
@@ -4,9 +4,8 @@
 
 killall clight
 
-if [ "$theme_name" = "light*" ]
-then
-    i3lock-fancy-dualmonitor&
-else
-    env XSECURELOCK_FONT="Iceland:pixelsize=29" XSECURELOCK_AUTH_BACKGROUND_COLOR="#162637" XSECURELOCK_PASSWORD_PROMPT=emoticon XSECURELOCK_SAVER=saver_mpv XSECURELOCK_LIST_VIDEOS_COMMAND="find ~/Videos -type l" XSECURELOCK_SHOW_DATETIME=1 XSECURELOCK_BLANK_DPMS_STATE=off xsecurelock&
-fi
+xset s 20 20
+i3lock-fancy-dualmonitor
+xset s off
+
+#env XSECURELOCK_FONT="Iceland:pixelsize=29" XSECURELOCK_AUTH_BACKGROUND_COLOR="#162637" XSECURELOCK_PASSWORD_PROMPT=emoticon XSECURELOCK_SAVER=saver_mpv XSECURELOCK_LIST_VIDEOS_COMMAND="find ~/Videos -type l" XSECURELOCK_SHOW_DATETIME=1 XSECURELOCK_BLANK_DPMS_STATE=off xsecurelock&
