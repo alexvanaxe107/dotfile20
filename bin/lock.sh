2e7c0ccb4e76a9f212d43c3e4fb6056a8e09dd23
Alexvanaxe <alexvanaxe@gmail.com>
2020-09-03 19:08:27 -0300

    Fixing something

--- bin/lock.sh
+++ bin/lock.sh
@@ -4,13 +4,14 @@
 
 killall clight
 background=$1
-xset s 20 20
+#xset s 20 20
 if [ "${background}" = "-d" ]
 then
-    i3lock-fancy-dualmonitor&
+    env XSECURELOCK_FONT="Iceland:pixelsize=29" XSECURELOCK_AUTH_BACKGROUND_COLOR="#162637" XSECURELOCK_PASSWORD_PROMPT=emoticon XSECURELOCK_SAVER=saver_mpv XSECURELOCK_LIST_VIDEOS_COMMAND="find ~/Videos -type l" XSECURELOCK_SHOW_DATETIME=1 XSECURELOCK_BLANK_DPMS_STATE=off xsecurelock&
+    #i3lock-fancy-dualmonitor&
 else
-    i3lock-fancy-dualmonitor
+    env XSECURELOCK_FONT="Iceland:pixelsize=29" XSECURELOCK_AUTH_BACKGROUND_COLOR="#162637" XSECURELOCK_PASSWORD_PROMPT=emoticon XSECURELOCK_SAVER=saver_mpv XSECURELOCK_LIST_VIDEOS_COMMAND="find ~/Videos -type l" XSECURELOCK_SHOW_DATETIME=1 XSECURELOCK_BLANK_DPMS_STATE=off xsecurelock
+    #i3lock-fancy-dualmonitor
 fi
-xset s off
+#xset s off
 
-#env XSECURELOCK_FONT="Iceland:pixelsize=29" XSECURELOCK_AUTH_BACKGROUND_COLOR="#162637" XSECURELOCK_PASSWORD_PROMPT=emoticon XSECURELOCK_SAVER=saver_mpv XSECURELOCK_LIST_VIDEOS_COMMAND="find ~/Videos -type l" XSECURELOCK_SHOW_DATETIME=1 XSECURELOCK_BLANK_DPMS_STATE=off xsecurelock&
