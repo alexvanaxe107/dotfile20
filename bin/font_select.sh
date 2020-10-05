a9528c8695ba516711e956007b94156f82baa5c7
Alexvanaxe <alexvanaxe@gmail.com>
2020-10-05 07:56:01 -0300

    Updating fonts and bookmarks

--- bin/font_select.sh
+++ bin/font_select.sh
@@ -76,7 +76,7 @@ case $CHOSEN in
     "War") font "American Stencil" Regular 9 10 12 1;;
     "Programming") font "Roboto Mono" Bold 8 8 11 1;;
     "Cursive") font "Z003" "Medium Italic" 13 12 15 2;;
-    "Soft") font "mononoki" Regular 11 11 13 2;;
+    "Soft") font "mononoki" Regular 11 11 15 2;;
     "80s") font "Press Start 2P" Regular 7 7 9 2;;
     "Cyberpunk") font "Braciola MS" "Regular Rg" 10 10 13 1;;
     "Pixel") font "Repetition Scrolling" Regular 9 10 13 1;;
