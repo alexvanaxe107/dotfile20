2dc5e8567066f8b8399d3ca3bad9f8cab3e53c4d
Alexvanaxe <alexvanaxe@gmail.com>
2020-11-26 17:36:51 -0300

    Big workarrounds

--- bin/font_select.sh
+++ bin/font_select.sh
@@ -8,11 +8,14 @@ set -o nounset
 # Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
 #set -o pipefail
 
-CHOSEN=$(printf "Day Original\\nNight Original\\nWasteland\\nElegant\\nElegant2\\nStock\\nRock\\nWar\\nMinimalist\\nNature\\nModern\\nFuturistic\\nWestern\\n80s\\nNeon\\nCyberpunk\\nPixel\\nOld Terminal\\nProgramming\\nSoft\\nBook\\nCursive\\nCartoon\\nCute\\nClear\\nSpace\\nNoir" | dmenu -i -y 16 -bw 2 -z 550 -l 30 -p "Change the font: ")
+CHOSEN=$(printf "Day Original\\nNight Original\\nWasteland\\nElegant\\nElegant2\\nStock\\nRock\\nWar\\nMinimalist\\nNature\\nModern\\nFuturistic\\nWestern\\n80s\\nNeon\\nCyberpunk\\nPixel\\nOld Terminal\\nProgramming\\nSoft\\nSofter\\nBook\\nCursive\\nCartoon\\nCute\\nClear\\nSpace\\nNoir\nNM-Comix zone\nNM-80s ScyFi\nNM-Japan" | dmenu -i -y 16 -bw 2 -z 550 -l 35 -p "Change the font: ")
 
 if [ -z "${CHOSEN}" ]; then
     exit
 fi
+
+NOT_MONO=$(echo $CHOSEN | cut -d '-' -f 1)
+    
 
 # Source the theme
 . ${HOME}/.config/bspwm/themes/bsp.cfg
@@ -26,8 +29,10 @@ font() {
     space=$6
 
     #Change the polybar
-    sed -i "s/font-1.*/font-1 = ${font_name}:style=${style}:pixelsize=${size1};${space}/" ${HOME}/.config/polybar/config
-    sed -i "s/font-1.*/font-1 = ${font_name}:style=${style}:pixelsize=${size1}/" ${HOME}/.config/polybar/config_simple
+    if [ ${NOT_MONO} != "NM" ]; then
+        sed -i "s/font-1.*/font-1 = ${font_name}:style=${style}:pixelsize=${size1};${space}/" ${HOME}/.config/polybar/config
+        sed -i "s/font-1.*/font-1 = ${font_name}:style=${style}:pixelsize=${size1}/" ${HOME}/.config/polybar/config_simple
+    fi
 
     #Change the dmenu font
     sed -i "s/DMENU_FN.*/DMENU_FN=\"${font_name}:style=${style}:size=${size2}\"/" ${HOME}/.config/bspwm/themes/bsp.cfg
@@ -40,16 +45,21 @@ font() {
     sed -i "s/font_size=.*/font_size=${size3}/" ${HOME}/.config/twmn/twmn.conf
 
     #Change the tint font
-    sed -i "s/font = Pomodoro.*/zont = Pomodoro 10/" ${HOME}/.config/tint2/tint2rc
-    sed -i "s/font =.*/font = ${font_name} ${style} ${size1}/" ${HOME}/.config/tint2/tint2rc
-    sed -i "s/zont = Pomodoro.*/font = Pomodoro 10/" ${HOME}/.config/tint2/tint2rc
+
+    if [ ${NOT_MONO} != "NM" ]; then
+        sed -i "s/font = Pomodoro.*/zont = Pomodoro 10/" ${HOME}/.config/tint2/tint2rc
+        sed -i "s/font =.*/font = ${font_name} ${style} ${size1}/" ${HOME}/.config/tint2/tint2rc
+        sed -i "s/zont = Pomodoro.*/font = Pomodoro 10/" ${HOME}/.config/tint2/tint2rc
+    fi
 
     #Change the lock screen font
     sed -i "s/FONT=.*/FONT=\"${font_name}\":pixelsize=$(($size3+7))/" ${HOME}/bin/imports/lock.sh
     
 
     for file in ${HOME}/.config/conky/*.conf; do
-        sed -i "s/font=.*/font='${font_name}:size=${size3}',/" ${file}
+        if [ ${NOT_MONO} != "NM" ]; then
+            sed -i "s/font=.*/font='${font_name}:size=${size3}',/" ${file}
+        fi
         sed -i "s/TITLEFONT/${font_name}/g" ${file}
     done
 
@@ -91,6 +101,7 @@ case $CHOSEN in
     "Programming") font "Roboto Mono" Bold 8 8 11 1;;
     "Cursive") font "Z003" "Medium Italic" 13 12 15 2;;
     "Soft") font "mononoki" Regular 11 11 14 2;;
+    "Softer") font "Lerton" Regular 10 10 13 2;;
     "80s") font "Press Start 2P" Regular 7 7 9 2;;
     "Cyberpunk") font "Braciola MS" "Regular Rg" 11 11 13 1;;
     "Pixel") font "Repetition Scrolling" Regular 9 10 13 1;;
@@ -100,7 +111,11 @@ case $CHOSEN in
     "Wasteland") font "Beccaria" Bold 12 12 16 1;;
     "Cartoon") font "Pointfree" Bold 9 9 12 1;;
     "Cute") font "Overpass Mono" Bold 10 10 12 0;;
-    "Space") font "Space Mono" Regular 9 10 12 3;;
+    "Space") font "Unispace" Bold 9 9 13 1;;
     "Noir") font "SyneMono-Regular" Regular 10 10 12 1;;
+    "NM-Comix zone") font "Bangers" Bold 10 10 12 0;;
+    "NM-80s ScyFi") font "Audiowide" Bold 9 9 12 0;;
+    "NM-Japan") font "Shojumaru" Bold 9 9 12 0;;
+    "teste") font "Shojumaru" Bold 10 10 12 0;;
     *) font "${CHOSEN}" Bold 9 10 11 0;;
 esac
