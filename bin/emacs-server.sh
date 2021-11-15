55f1dac62d86412f63658f5aaeb7fb3bd8744068
Alexvanaxe <alexvanaxe@gmail.com>
2021-11-15 00:38:13 -0300

    Typescript support added

--- bin/emacs-server.sh
+++ bin/emacs-server.sh
@@ -7,6 +7,4 @@ if [ "${theme_name}" = "day" ]; then
 fi
 
 
-exec dbus-launch --exit-with-session emacs --daemon -bg "#000000" -fg "#ffffff" -mm --debug-init -l $HOME/.emacs.d/desktop.el 
-
-# emacs -bg "#000000" -fg "#ffffff" --execute "(with-eval-after-load 'doom-themes (load-theme 'doom-city-lights t))"
+emacs --daemon -bg "#000000" -fg "#ffffff" -mm --debug-init
