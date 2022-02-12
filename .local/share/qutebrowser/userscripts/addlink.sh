de522fa3f22a9a9076daba4170888f0b17fdafe2
Alexvanaxe <alexvanaxe@gmail.com>
2022-02-12 19:21:41 -0300

    Some updates

--- .local/share/qutebrowser/userscripts/addlink.sh
+++ .local/share/qutebrowser/userscripts/addlink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 URL=$(echo "$QUTE_URL" | sed "s/\&.*//")
-(echo "youtube-dl --cookies ~/Downloads/cookies.txt \"$URL\"" >> ~/download.sh) || exit 0
+(echo "yt-dlp --cookies ~/Downloads/cookies.txt \"$URL\"" >> ~/download.sh) || exit 0
 
 NUM=$(nl download.sh | tail -n 1 | awk '{print $1}')
 notify-send -u normal  "Clip Added" "Added $NUM link!"
