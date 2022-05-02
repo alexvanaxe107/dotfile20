aab0aff126a3412c529c7c88fb3ac8a78b949bf2
Alexvanaxe <alexvanaxe@gmail.com>
2022-05-02 19:30:26 -0300

    Configs from qtile

---
+++ .config/qtile/hooks.py
@@ -0,0 +1,9 @@
+import os
+import subprocess
+from libqtile import hook
+
+
+@hook.subscribe.startup_once
+def autostart():
+    home = os.path.expanduser('~')
+    subprocess.Popen([home + '/.config/qtile/autostart.sh'])
