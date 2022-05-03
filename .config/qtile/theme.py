9b3d75ea77b3de90d1b199df86d7c26a38ec0394
Alexvanaxe <alexvanaxe@gmail.com>
2022-05-03 15:25:33 -0300

    Configs from qtile

--- .config/qtile/theme.py
+++ .config/qtile/theme.py
@@ -1,25 +1,25 @@
 theme_name = "day"
 
 bar_configs = dict(
-    font='Erica Type',
+    font="Targa MS",
     fontsize=12,
     padding=8
     )
 
 colors = {
-    "active_border": '#102528',
-    "border_normal": '#4C7788',
-    "bar_background": '#8CB3BC87',
-    "bar_background_dark": '#0F121F87',
-    "bar_foreground": '#8CB3BC',
+    "active_border": '#635550',
+    "border_normal": '#DDCDCC',
+    "bar_background": '#EEEEDD87',
+    "bar_background_dark": '#46333387',
+    "bar_foreground": '#EEEEDD',
     "bar_foreground2": '#ffffff',
-    "bar_hl_act": '#4D5664',
-    "bar_hl_normal": '#8CB3BC',
-    "cpu_bk": '#0F121F',
-    "memory_bk": '#325D68',
-    "swap_bk": '#262A2A',
-    "clock_bk": '#4D5664',
-    "vol_bk": '#4C7788',
+    "bar_hl_act": '#F7EEEE',
+    "bar_hl_normal": '#EEEEDD',
+    "cpu_bk": '#463333',
+    "memory_bk": '#D4B5A9',
+    "swap_bk": '#8B746C',
+    "clock_bk": '#F7EEEE',
+    "vol_bk": '#DDCDCC',
 }
 
 values = {
