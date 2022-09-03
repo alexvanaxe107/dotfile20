2145713a0beccd9a4be110240bda9205706292a0
Alexvanaxe <alexvanaxe@gmail.com>
2022-09-03 14:48:48 -0300

    wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,28 @@
+local wezterm = require 'wezterm'
+
+toReturn = {}
+
+theme_name = "bluet"
+if os.getenv("theme_name") == "day" then
+    font_name = "JetBrains Mono"
+elseif os.getenv("theme_name") == "night" then
+    font_name = "Iosevka"
+else
+    font_name = "JetBrains Mono"
+end
+
+toReturn['color_scheme'] = theme_name
+toReturn['font'] = wezterm.font(font_name)
+toReturn['font_size'] = 11
+toReturn['window_decorations'] = "NONE"
+toReturn['hide_tab_bar_if_only_one_tab'] = true
+
+if os.getenv("theme_name") == "day" then
+    toReturn['window_background_opacity'] = 0.9
+elseif os.getenv("theme_name") == "night" then
+    toReturn['window_background_opacity'] = 0.6
+else
+    toReturn['window_background_opacity'] = 1.0
+end
+
+return toReturn
