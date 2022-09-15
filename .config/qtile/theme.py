2022354240312595e7e8d72a792ba8b75892b92d
Alexvanaxe <alexvanaxe@gmail.com>
2022-09-15 13:09:52 -0300

    Django and ng comp, and other files

--- .config/qtile/theme.py
+++ .config/qtile/theme.py
@@ -1,18 +1,27 @@
-theme_name = "light"
+theme_name = "day"
 
 bar_configs = dict(
-    font='VT323',
-    fontsize=15,
-    padding=2
+    font='Erica Type',
+    fontsize=12,
+    padding=8
     )
 
 colors = {
-    "active_border": '#ffffff',
-    "border_normal": '#555555',
-    "bar_background": '#000033',
-    "bar_foreground": '#eeeeee',
+    "active_border": '#FFFFFF',
+    "border_normal": '#4B4B4B',
+    "bar_background": '#FFFFFF87',
+    "bar_background_dark": '#11111187',
+    "bar_foreground": '#FFFFFF',
+    "bar_foreground2": '#ffffff',
+    "bar_hl_act": '#A2A2A2',
+    "bar_hl_normal": '#FFFFFF',
+    "cpu_bk": '#111111',
+    "memory_bk": '#4B4B4B',
+    "swap_bk": '#2A2A2A',
+    "clock_bk": '#4B4B4B',
+    "vol_bk": '#6F6F6F',
 }
 
 values = {
-    "gap_size": 0,
+    "gap_size": 15,
 }
--- .vim/configs/theme.vim
+++ .vim/configs/theme.vim
@@ -1,12 +1,12 @@
 "if (has("termguicolors"))
   set termguicolors
 "endif
-set background=dark
+set background=light
 
 source ~/.vim/configs/theme_config.vim
 
-colorscheme base16-greenscreen
-let g:airline_theme="base16_greenscreen"
+colorscheme gruvbox
+let g:airline_theme="gruvbox"
 "hi Normal ctermbg=None
 "let g:airline_theme="gruvbox"
 
