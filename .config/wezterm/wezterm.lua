dfb4dc3ccfbe5334aa85052988bbce6df428ac0f
Alexvanaxe <alexvanaxe@gmail.com>
2022-10-13 14:03:49 -0300

    A lot of improvements

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,14 +1,23 @@
 local wezterm = require 'wezterm'
 
+local function isempty(s)
+  return s == nil or s == ''
+end
+
 toReturn = {}
 
-theme_name = "Azu (Gogh)"
+theme_name = "Belafonte Day"
+custom_term_font = ""
 if os.getenv("theme_name") == "day" then
     font_name = "JetBrains Mono"
 elseif os.getenv("theme_name") == "night" then
     font_name = "Iosevka"
 else
     font_name = "JetBrains Mono"
+end
+
+if not isempty(custom_term_font) then
+    font_name = custom_term_font
 end
 
 toReturn['color_scheme'] = theme_name
@@ -20,7 +29,7 @@ toReturn['hide_tab_bar_if_only_one_tab'] = true
 if os.getenv("theme_name") == "day" then
     toReturn['window_background_opacity'] = 0.9
 elseif os.getenv("theme_name") == "night" then
-    toReturn['window_background_opacity'] = 0.6
+    toReturn['window_background_opacity'] = 0.78
 else
     toReturn['window_background_opacity'] = 1.0
 end
