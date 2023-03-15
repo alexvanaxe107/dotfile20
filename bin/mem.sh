bd9cf97e90ebc6c6ba21addff5f0e5686f7e0a7a
AVA <alexvanaxe@gmail.com>
2023-03-15 14:34:09 -0300

    Adding notifications

--- bin/mem.sh
+++ bin/mem.sh
@@ -1,8 +1,9 @@
 #! /bin/dash
 
 mem(){
+  mem_per="$(free -m | awk '/Mem/ {printf "%.2f", $3/$2*100 }')"
   mem=`free | awk '/Mem/ {printf "%d MiB/%d MiB\n", $3 / 1024.0, $2 / 1024.0 }'`
-  echo $mem
+  echo "$mem ($mem_per)%"
 }
 
 mem
