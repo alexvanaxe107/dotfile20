6b66a21377f7e4d59d12ba1189e1eb563a14b754
AVA <alexvanaxe@gmail.com>
2023-03-21 17:49:26 -0300

    Some fixes

--- bin/play_radio.sh
+++ bin/play_radio.sh
@@ -199,7 +199,7 @@ play_quality(){
     notify-send -u normal  "Trying to play" "The media will be played soon... wait a little and enjoy."
 
     echo "mpv \"$result\"" > ${PLAY_BKP};
-    #mpv "$result" --ytdl-format=${choosen_quality}
+    #mpv "$result" --ytdl-format=${choosen_quality} This is giving an error at the moment. Try again another time
     mpv "$result"
 
     notify-send -u normal  "Done" "Hopefully your media was played =/"
