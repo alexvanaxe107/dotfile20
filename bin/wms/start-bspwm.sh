aa4f82d697e6ca561eee8662b887b50d90a1cd90
AlexVanAxe <alexvanaxe@gmail.com>
2023-04-12 15:26:44 -0300

    Theme day changed

--- bin/wms/start-bspwm.sh
+++ bin/wms/start-bspwm.sh
@@ -2,15 +2,24 @@
 
 DP="DisplayPort-2"
 HDMI="HDMI-A-0"
+chosen="$DESK_ENV"
 
 display_manager.sh -o "$DP $HDMI"
+if [ "${chosen}" == "Work" ]; then
+    xrandr --output $DP --mode "1920x1080"
+fi
+if [ "${chosen}" == "Game" ]; then
+    xrandr --output $DP --mode "2560x1440"
+fi
 display_manager.sh -p "$DP"
+display_manager.sh -r "$HDMI" right
+display_manager.sh -o "$DP $HDMI"
 
 #display_manager.sh -r "HDMI-2" "left"
-#xrandr --output $DP --mode "2560x1440"
 #xrandr --output $HDMI --scale 1.5x1.5
 
 systemd --user
 
 #exec dbus-launch --exit-with-session bspwm
 exec bspwm
+
