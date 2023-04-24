ffd0f44505fe0b674631ea84b426c5e3b6af57dd
Alexvanaxe <alexvanaxe@gmail.com>
2023-04-24 18:20:37 -0300

    Changing somethings

--- bin/start.sh
+++ bin/start.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
+is_openbox=0
+if [ ! -z "$(command -v openbox)" ]; then
+    is_openbox=1
+else
+    echo "In case of problems in resolution or any other issues, please install openbox"
+fi
+
 DP="DisplayPort-2"
 game="$(which $1)"
 tmp_start=/tmp/start_tmp.sh
 cp /home/alexvanaxe/bin/start_tmp.sh ${tmp_start}
-dim=$(printf "%s\n%s\n%s\n" "3840x2160" "2560x1440" "1920x1080" | fzf)
+
+dimensions="$(cat $HOME/.config/wm/monitor_options.conf)"
+dim=$(printf "$dimensions" | fzf)
+dim=$(awk '{print $1}' <<< "${dim}")
+
+echo "Iniciando $1 em modo $dim. Enjoy!"
+sleep 2
 
 echo -e "xrandr --output $DP --mode '$dim'" >> ${tmp_start}
 echo -e "xrandr --output $DP --set TearFree on" >> ${tmp_start}
-openbox="$(which openbox)"
 
-if [ "$1" == "steam" ]; then
-    echo -e "${openbox} &\ngamemoderun ${game}" -gamepadui >> ${tmp_start}
-else
-    echo -e "${openbox} &\ngamemoderun ${game}" >> ${tmp_start}
+if [ ${is_openbox} == 1 ]; then
+    openbox="$(which openbox) &\n "
 fi
 
+if [ "$1" == "steam" ]; then
+    echo -e "${openbox}gamemoderun ${game}" -gamepadui >> ${tmp_start}
+else
+    echo -e "${openbox}gamemoderun ${game}" >> ${tmp_start}
+fi
+xsetroot -cursor_name left_ptr
 xinit ${tmp_start} -- :1 vt$XDG_VTNR || exit 1
--- bin/teste.sh
+++ bin/teste.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-htop=$(monitors_info.sh -d | awk 'BEGIN {FS="x"} {if ($2 > TESTE) TESTE=$2;} END {print TESTE}')
-
-optimal_rescale="$(monitors_info.sh -D)x${htop}"
-
-printf $optimal_rescale
+if [ -z "$(xrandr)" ]; then
+    echo "Sem xrandr"
+else
+    echo "Com xrandr"
+fi
