c144022a4ef28bf51fc6a942c8ae6912ade7c067
AlexVanAxe <alexvanaxe@gmail.com>
2023-05-17 10:15:57 -0300

    Some configs

--- bin/gaming_mode.sh
+++ bin/gaming_mode.sh
@@ -2,12 +2,12 @@
 
 setcap 'CAP_SYS_NICE=eip' /usr/bin/gamescope
 
-echo 0 > /proc/sys/vm/compaction_proactiveness
-echo 1048576 > /proc/sys/vm/min_free_kbytes
-echo 10 > /proc/sys/vm/swappiness
-echo 5 > /sys/kernel/mm/lru_gen/enabled
-echo 0 > /proc/sys/vm/zone_reclaim_mode
-echo 1 > /proc/sys/vm/page_lock_unfairness
+#echo 0 > /proc/sys/vm/compaction_proactiveness
+#echo 1048576 > /proc/sys/vm/min_free_kbytes
+#echo 10 > /proc/sys/vm/swappiness
+#echo 5 > /sys/kernel/mm/lru_gen/enabled
+#echo 0 > /proc/sys/vm/zone_reclaim_mode
+#echo 1 > /proc/sys/vm/page_lock_unfairness
 
 # *** Note that if your game uses TCMalloc (e.g., Dota 2 and CS:GO) then it is not recommended to disable THP
 #echo never > /sys/kernel/mm/transparent_hugepage/enabled
@@ -16,12 +16,12 @@ echo 1 > /proc/sys/vm/page_lock_unfairness
 # *** Comment until here
 
 # *** You may want to comment the bellow session as well
-echo 0 > /proc/sys/kernel/sched_child_runs_first
-echo 1 > /proc/sys/kernel/sched_autogroup_enabled
-echo 500 > /proc/sys/kernel/sched_cfs_bandwidth_slice_us
-echo 1000000 > /sys/kernel/debug/sched/latency_ns
-echo 500000 > /sys/kernel/debug/sched/migration_cost_ns
-echo 500000 > /sys/kernel/debug/sched/min_granularity_ns
-echo 0 > /sys/kernel/debug/sched/wakeup_granularity_ns
-echo 8 > /sys/kernel/debug/sched/nr_migrate
+#echo 0 > /proc/sys/kernel/sched_child_runs_first
+#echo 1 > /proc/sys/kernel/sched_autogroup_enabled
+#echo 500 > /proc/sys/kernel/sched_cfs_bandwidth_slice_us
+#echo 1000000 > /sys/kernel/debug/sched/latency_ns
+#echo 500000 > /sys/kernel/debug/sched/migration_cost_ns
+#echo 500000 > /sys/kernel/debug/sched/min_granularity_ns
+#echo 0 > /sys/kernel/debug/sched/wakeup_granularity_ns
+#echo 8 > /sys/kernel/debug/sched/nr_migrate
 # *** Comment until here
--- bin/imports/lock.sh
+++ bin/imports/lock.sh
@@ -1,2 +1,2 @@
-FONT="Share Tech Mono":pixelsize=20
+FONT="CaskaydiaCove Nerd Font Mono":pixelsize=20
 SAVER=saver_mpv
