652ac7f4713f638ef692d32814e7aa16ae93b697
AlexVanAxe <alexvanaxe@gmail.com>
2023-07-09 16:22:06 -0300

    Creating build script

--- builder.sh
+++ builder.sh
@@ -1,1 +1,18 @@
 echo "Making desktop"
+
+unset PATH
+
+for p in $baseInputs $buildInputs; do
+  if [ -d $p/bin ]; then
+    export PATH="$p/bin${PATH:+:}$PATH"
+  fi
+  if [ -d $p/lib/pkgconfig ]; then
+    export PKG_CONFIG_PATH="$p/lib/pkgconfig${PKG_CONFIG_PATH:+:}$PKG_CONFIG_PATH"
+  fi
+done
+
+echo "Creating output"
+mkdir $out
+
+echo "Copying files."
+cp -rf $src/* $out
