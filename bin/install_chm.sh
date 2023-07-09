fe14393981f75092eeb1a27575ce75b854e75082
AlexVanAxe <alexvanaxe@gmail.com>
2023-07-09 18:08:18 -0300

    Changing themes and imports location

--- bin/install_chm.sh
+++ bin/install_chm.sh
@@ -1,10 +1,17 @@
 # We will not include the env bash because it will be executed in the nix install
 # But it can be executed at any moment, when we want to override the unchangable files
 # for new ones. Here must have only 'static' files.
+NIX_PROFILE_DIR="$NIX_USER_PROFILE_DIR/profile"
+AVA_TEMPLATES="$NIX_PROFILE_DIR/share/avatemplates"
 
-INSTALL_DIR="$0"
+USER_TEMPLATE="$HOME/templates"
 
-echo $INSTALL_DIR
+if [ -f "$USER_TEMPLATE" ]; then
+    echo "Template already exists. Not touching it."
+else
+    echo "Creating symlink to template"
+    ln -s $AVA_TEMPLATES $USER_TEMPLATE
+fi
 
 echo "Copying the configuration files"
 #cp -rf $HOME/configs/* $HOME
