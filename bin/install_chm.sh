178e7909e2322ba17a68e9e318c0f9023db4f976
AlexVanAxe <alexvanaxe@gmail.com>
2023-07-09 23:20:49 -0300

    Adding force and other little things

--- bin/install_chm.sh
+++ bin/install_chm.sh
@@ -2,9 +2,13 @@
 # But it can be executed at any moment, when we want to override the unchangable files
 # for new ones. Here must have only 'static' files.
 
-umask 022
+umask 022 # NIX_PROFILE_DIR="$NIX_USER_PROFILE_DIR/profile"
 
-# NIX_PROFILE_DIR="$NIX_USER_PROFILE_DIR/profile"
+force="$1"
+
+if [ "$force" == "force" ];  then
+    echo "Force is enabled"
+fi
 
 # Allow unfree
 NIXPKGS_ALLOW_UNFREE=1
@@ -20,6 +24,9 @@ USER_DOCS_NIX="$HOME/Documents/Projects/nixconfs/"
 
 if [ -L "$USER_TEMPLATE" ]; then
     echo "Template already exists. Not touching it."
+    if [ "$force" == "force" ];  then
+        ln -s $AVA_TEMPLATES $USER_TEMPLATE
+    fi
 else
     echo "Creating symlink to template"
     ln -s $AVA_TEMPLATES $USER_TEMPLATE
@@ -27,6 +34,9 @@ fi
 
 if [ -L "$USER_CONFIGS" ]; then
     echo "Config already exists. Not touching it."
+    if [ "$force" == "force" ];  then
+        ln -s $AVA_CONFIGS $USER_CONFIGS
+    fi
 else
     echo "Creating symlink to configs"
     ln -s $AVA_CONFIGS $USER_CONFIGS
@@ -36,14 +46,28 @@ echo "Copying the configuration files"
 cp --no-preserve=all -rf $HOME/configs/* $HOME
 cp --no-preserve=all -rf $HOME/configs/.* $HOME
 chmod 700 $HOME/.config/bspwm/bspwmrc
+chmod 700 $HOME/.config/sxiv/exec/*
 
-echo "Cloning nixos conigs"
 if [ ! -d "$USER_DOCS_NIX" ]; then
+    echo "Cloning nixos conigs"
     git clone "https://github.com/alexvanaxe107/nixconfs.git" "$USER_DOCS_NIX"
+else
+    if [ "$force" == "force" ];  then
+        git clone "https://github.com/alexvanaxe107/nixconfs.git" "$USER_DOCS_NIX"
+    fi
 fi
 
-echo "installing the packages"
-nix-env -f '<nixpkgs>' -r -iA userPackages
+
+if [ ! -f $HOME/.config/wm/install.lock ]; then
+    echo "installing the packages"
+    nix-env -f '<nixpkgs>' -r -iA userPackages
+else
+    if [ "$force" == "force" ];  then
+        echo "installing the packages"
+        nix-env -f '<nixpkgs>' -r -iA userPackages
+    fi
+fi
+echo "install.lock" > $HOME/.config/wm/install.lock
 
 # Configuring settings
 # -- Configurar git para global store
@@ -52,8 +76,25 @@ git config --global credential.helper store
 git config --global http.postBuffer 157286400
 
 # -- configurar o packer. Rodar:
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+if [ ! -d ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]; then
+    echo "oOnfiguring packer"
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+else
+    if [ "$force" == "force" ];  then
+        echo "oOnfiguring packer"
+        git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+         ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+    fi
+fi
 
 # -- configurar o tmux
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    echo "Configuring tmux"
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+else
+    if [ "$force" == "force" ];  then
+        echo "Configuring tmux"
+        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    fi
+fi
