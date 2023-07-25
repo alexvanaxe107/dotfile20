65a46d102e8e686700ddc9247a63a55cbbc66dde
avasl <alexvanaxe@gmail.com>
2023-07-25 11:38:17 -0300

    Some improvments to work with the configs

---
+++ update_configs.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+files="$(find ./share/configs/ -type f)"
+files="${files//\.\/share\/configs\//}"
+diff_files=""
+
+echo "The files below are changed on your config:"
+echo
+while IFS= read -r file; do
+    result=$(diff "$HOME/$file" "./share/configs/$file")
+    if [ ! -z "$result" ];then
+        echo $file
+        diff_files=$(echo -e "$file\n$diff_files")
+    fi
+done <<< "$files" 
+
+echo
+echo "What do you want to do?"
+option=$(echo -e "update\noverdide\nvimdiff\nviewdiffs" | fzf --height 30%)
+
+if [ "${option}" == "vimdiff" ]; then
+    while IFS= read -r file; do
+        nvim -d $HOME/$file ./share/configs/$file
+    done <<< "$diff_files" 
+fi
+
