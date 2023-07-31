1ce092d0dec8c7f7a819344bfa464103d00f358b
avasl <alexvanaxe@gmail.com>
2023-07-31 14:16:43 -0300

    Making this a flake project

--- ava-chamaleon.nix
+++ ava-chamaleon.nix
@@ -1,24 +1,23 @@
-with import <nixpkgs> {}; # bring all of Nixpkgs into scope
-
-stdenv.mkDerivation rec {
+{ pkgs }:
+pkgs.stdenv.mkDerivation rec {
   name = "ava-chamaleon";
   version = "0.7.2";
 
-  src = fetchFromGitHub {
+  src = pkgs.fetchFromGitHub {
     owner = "alexvanaxe107";
     repo = "dotfile20";
     rev = "nix_pack";
 
-    sha256 = "sha256-ih0AerJ3hu5Ltzqh/lDWfc6JDFc2Yu96TXagFJm5hPI=";
+    sha256 = "sha256-Y0SU9zoRGcYi/UTc10lqbJa090NIsefCGCVSS+adV3I=";
   };
 
-  builder = "${bash}/bin/bash";
+  builder = "${pkgs.bash}/bin/bash";
   args = [ "${src}/builder.sh" ];
 #  args = [ ./builder.sh ];
 
-  baseInputs = [ coreutils binutils.bintools ];
+  baseInputs = [ pkgs.coreutils pkgs.binutils.bintools ];
 
-  meta = with lib; {
+  meta = with pkgs.lib; {
     description = "The files for the desktop enviromnetn";
     license = licenses.mit;
     maintainers = with maintainers; [ "alexvanaxe107" ];
--- flake.nix
+++ flake.nix
@@ -1,9 +1,13 @@
 {
   description = "The desktop files";
 
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
+
   outputs = { self, nixpkgs }: {
 
-    packages.x86_64-linux.default = ./ava-chamaleon.nix;
+    packages.x86_64-linux.default = 
+    with import nixpkgs { system = "x86_64-linux"; };
+    import ./ava-chamaleon.nix {pkgs={inherit stdenv lib fetchFromGitHub coreutils binutils;};};
 
   };
 }
