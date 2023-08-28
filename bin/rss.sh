b213b0b45e72924b52951f4de8fad626c35237d9
avasl <alexvanaxe@gmail.com>
2023-08-28 14:52:45 -0300

    Update on rss

--- bin/rss.sh
+++ bin/rss.sh
@@ -2,10 +2,11 @@
 
 #xmlstarlet select --template --value-of /rss/channel/item/title --nl rss.xml
 
-CONFIG_FILE="$HOME/.config/wm/rss"
+CONFIG_FILE="$HOME/.config/newsboat/urls"
 
 fetch_rss(){
-    rsstail -u $(cat $CONFIG_FILE) -1 -N -n 5 -Z "News:"
+    local news_from="$(cat $CONFIG_FILE | shuf | head -n 1)"
+    rsstail -u "$news_from" -1 -N -n 7 -Z "News:"
 }
 
 
--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     with import nixpkgs { system = "x86_64-linux"; };
     stdenv.mkDerivation rec {
       name = "ava-chamaleon";
-      version = "0.7.10";
+      version = "0.7.11";
 
       src = self;
 
