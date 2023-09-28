55e89e4cbdf87dbf5a3966b8ce2bfc7d6f05e1ee
avasl <alexvanaxe@gmail.com>
2023-09-28 17:29:16 -0300

    Updating the configs

--- share/configs/.config/home-manager/ava.nix
+++ share/configs/.config/home-manager/ava.nix
@@ -19,7 +19,7 @@
         pkgs.yt-dlp
         pkgs.sxiv
         pkgs.imagemagick
-        # picom = self.picom;
+        pkgs.picom
         pkgs.playerctl
         pkgs.cli-visualizer
         pkgs.pulseaudio
@@ -86,21 +86,21 @@
         # Here we will abuse a little and take confort by adding this code Here
         # to avoid the creation of other repo. If it begins to happen a lot (hope not)
         # then I fix it.
-        (pkgs.picom.overrideAttrs (oldAttrs: rec {
-          pname = "picom-animations";
-          version = "unstable-2022-08";
-          src = pkgs.fetchFromGitHub {
-            owner = "dccsillag";
-            repo = "picom";
-            rev = "51b21355696add83f39ccdb8dd82ff5009ba0ae5";
-            sha256 = "sha256-crCwRJd859DCIC0pEerpDqdX2j8ZrNAzVaSSB3mTPN8=";
-          };
+        #(pkgs.picom.overrideAttrs (oldAttrs: rec {
+        #  pname = "picom-animations";
+        #  version = "unstable-2022-08";
+        #  src = pkgs.fetchFromGitHub {
+        #    owner = "dccsillag";
+        #    repo = "picom";
+        #    rev = "51b21355696add83f39ccdb8dd82ff5009ba0ae5";
+        #    sha256 = "sha256-crCwRJd859DCIC0pEerpDqdX2j8ZrNAzVaSSB3mTPN8=";
+        #  };
 
-          meta = with pkgs.lib; {
-            description = "A fork of picom featuring animations and improved rounded corners.";
-            homepage = "https://github.com/jonaburg/picom";
-            maintainers = with maintainers; oldAttrs.meta.maintainers ++ [ ];
-          };
-        }))
+        #  meta = with pkgs.lib; {
+        #    description = "A fork of picom featuring animations and improved rounded corners.";
+        #    homepage = "https://github.com/jonaburg/picom";
+        #    maintainers = with maintainers; oldAttrs.meta.maintainers ++ [ ];
+        #  };
+        #}))
     ];
 }
