dc53a5f318480cbc9a1e305ba021f469306ef966
avasl <alexvanaxe@gmail.com>
2023-09-29 13:13:55 -0300

    Emacs configs for nix

--- bin/start_picom.sh
+++ bin/start_picom.sh
@@ -5,12 +5,12 @@ extra_configs=$2
 
 if [[ "${mode}" != "emacs" ]]; then
     echo "Starting normal"
-    picom $2 --experimental-backends -b >> /tmp/picom.log 3>&1 &
+    picom $2 -b >> /tmp/picom.log 3>&1 &
 elif [ "${mode}" == "emacs" ]; then
     echo "Starting emacs"
     isrunning="$(ps aux | grep -E "[p]icom.*config.*emacs")"
     if [ -z "${isrunning}" ]; then
-        picom $2 --experimental-backends --config $HOME/.config/picom/picom-emacs.conf -b  >> /tmp/picom.log 2>&1 &
+        picom $2 --config $HOME/.config/picom/picom-emacs.conf -b  >> /tmp/picom.log 2>&1 &
     fi
 fi
 #picom
