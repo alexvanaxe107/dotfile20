e2a3b9a12183d590aa15aba65ad42b5763a7c9eb
avasl <alexvanaxe@gmail.com>
2023-10-04 11:16:19 -0300

    Updating

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     with import nixpkgs { system = "x86_64-linux"; };
     stdenv.mkDerivation rec {
       name = "ava-chamaleon";
-      version = "0.7.22";
+      version = "0.7.23";
 
       src = self;
 
