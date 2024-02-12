a0a07579d0f0fad77c4778fbd1bf34faf87beb10
avasl <alexvanaxe@gmail.com>
2024-02-11 21:08:26 -0300

    Solving problem of pywall and tmux definitively

--- bin/pywal.sh
+++ bin/pywal.sh
@@ -2,7 +2,13 @@
 
 source $HOME/.config/wm/bspwm.conf
 
-if [ "${wal_enabled}" == "1" ] && [ "${PY_ENABLED}" != "1" ]; then
-    export PY_ENABLED="1"
-    (cat ~/.cache/wal/sequences &)
+# If running from tmux, we don't want to enable wal (It is already enabled from 
+# the bash)
+
+is_tmux="$TMUX_PANE"
+
+if [ "${wal_enabled}" == "1" ]; then
+    if [ -z "${is_tmux}" ]; then
+        (cat ~/.cache/wal/sequences &)
+    fi
 fi
