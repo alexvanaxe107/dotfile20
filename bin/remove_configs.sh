cd2559fdc683075464b04e2dd19be4b8a4a854e4
avasl <alexvanaxe@gmail.com>
2024-02-15 16:08:39 -0300

    fixing remove configs

--- bin/remove_configs.sh
+++ bin/remove_configs.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-files="$(find ./share/configs/ -type f)"
-files="${files//\.\/share\/configs\//}"
+files="$(find ~/configs/ -type f)"
+#files="${files//\.\/share\/configs\//}"
 diff_files=""
 
 echo "The files below are changed on your config:"
 echo
 while IFS= read -r file; do
     #mkdir -p $(dirname /home/alexvanaxe/config_bkp/${file})
-    rm "/home/alexvanaxe/$file"
+    echo "rm "$file""
 done <<< "$files" 
