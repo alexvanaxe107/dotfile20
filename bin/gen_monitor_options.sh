c3402bbc7ddcda962feb9654f0fae30cb71699ff
AlexVanAxe <alexvanaxe@gmail.com>
2024-02-24 23:37:44 -0300

    Receiving monitor on parametter

--- bin/gen_monitor_options.sh
+++ bin/gen_monitor_options.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-monitor="$(monitors_info.sh -m | dmenu -p "Gen options for which monitor?")"
+monitor="$1"
 
 xrandr | grep -P "\d+x\d+" | cut --output-delimiter=":" -s -d ' ' -f 1,4 | awk '{{if ($1 != "") val=$1}; {if ($1 == "") print val "x" $2}}' FS=':' | grep "${monitor}" | awk '{printf "%sx%s 1:%0.0f\n", $2,$3,$2/$3*10}' FS=x | head -n 10 > $HOME/.config/wm/monitor_options.conf
