e779c378a266941c376f37b6288f8857fa846242
AlexVanAxe <alexvanaxe@gmail.com>
2024-02-24 23:12:51 -0300

    Ok.. lets see

--- machines/gaming/.config/home-manager/ava.nix
+++ machines/gaming/.config/home-manager/ava.nix
@@ -6,15 +6,12 @@
     # Let Home Manager install and manage itself.
     programs.home-manager.enable = true;
 
-    programs.steam = {
-      enable = true;
-    };
-
     programs.java.enable = true; 
-    programs.steam.gamescopeSession.enable = true;
 
     home.packages = [ 
         pkgs.openbox
+        pkgs.steam
+        pkgs.gamescope
     ];
 }
 
