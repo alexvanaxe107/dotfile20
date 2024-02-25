7098af7f3609b7ef3e03118c4f979abaf966982b
AlexVanAxe <alexvanaxe@gmail.com>
2024-02-24 23:07:53 -0300

    Adding steam

--- machines/gaming/.config/home-manager/ava.nix
+++ machines/gaming/.config/home-manager/ava.nix
@@ -6,6 +6,15 @@
     # Let Home Manager install and manage itself.
     programs.home-manager.enable = true;
 
+    programs.steam = {
+      enable = true;
+    };
+
+    programs.java.enable = true; 
+    programs.steam.package = pkgs.steam.override { withJava = true; };
+
+    programs.steam.gamescopeSession.enable = true;
+
     home.packages = [ 
         pkgs.openbox
     ];
