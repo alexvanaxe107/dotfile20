748dbd92f0f594b9e0397895310b47e4380d2e8b
avasl <alexvanaxe@gmail.com>
2024-02-28 19:01:52 -0300

    Adjusting monitors

--- machines/gaming/.config/wm/xorg_local.sh
+++ machines/gaming/.config/wm/xorg_local.sh
@@ -2,9 +2,9 @@
 
 # Configs used for the local configuragion of the specific computer
 
-HOST_NAME="$HOST"
+HOST_NAME="$HOSTNAME"
 
-if [ "$HOST" == "persistence" ]; then
+if [ "$HOST_NAME" == "persistence" ]; then
     echo "DP-3"
     exit 0
 fi
