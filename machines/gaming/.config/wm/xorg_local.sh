51253a74c33afc7347bf4701657b6422cf735078
avasl <alexvanaxe@gmail.com>
2024-02-29 10:02:02 -0300

    Adjusting monitor dimension

--- machines/gaming/.config/wm/xorg_local.sh
+++ machines/gaming/.config/wm/xorg_local.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-# Configs used for the local configuragion of the specific computer
-
 HOST_NAME="$HOSTNAME"
 CHOSE=$1
 
 if [ "$HOST_NAME" == "persistence" ]; then
     if [ "${CHOSE}" == "yes" ]; then
-        echo "DisplayPort-0"
+        echo "DisplayPort-2\nHDMI-A-0 | fzf"
     else
         echo "DP-3"
     fi
