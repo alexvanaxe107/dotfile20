793e4f9e6cd1509b1d45cb6304b83addef9863b8
avasl <alexvanaxe@gmail.com>
2024-02-28 21:12:50 -0300

    Adjusgint

--- machines/gaming/bin/start_tmp.sh
+++ machines/gaming/bin/start_tmp.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-DP="DisplayPort-2"
-HDMI="HDMI-A-0"
+#DP="DisplayPort-2"
+#HDMI="HDMI-A-0"
 
 
 #display_manager.sh -o "$DP $HDMI"
 #display_manager.sh -p "$DP"
-xrandr --output HDMI-A-0 --off
+#xrandr --output HDMI-A-0 --off
 
 
 game="$(which $1)"
