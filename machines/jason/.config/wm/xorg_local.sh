e23db283b1e684c2801c17380bbbd2f243cc4a25
avasl <alexvanaxe@gmail.com>
2024-03-06 13:25:20 -0300

    Removing uneeded stuffs from jason

--- machines/jason/.config/wm/xorg_local.sh
+++ machines/jason/.config/wm/xorg_local.sh
@@ -3,11 +3,7 @@
 # Configs used for the local configuragion of the specific computer
 
 DP="DisplayPort-2"
-HDMI="HDMI-A-0"
 
 xrandr --output $DP --set TearFree on
-xrandr --output $HDMI --set TearFree on
 
 display_manager.sh -p "$DP"
-display_manager.sh -r "$HDMI" "left"
-display_manager.sh -o "$DP $HDMI"
