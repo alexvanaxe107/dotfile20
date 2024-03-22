087aae9344d008561161ad54924a74e3ad873273
AlexVanAxe <alexvanaxe@gmail.com>
2024-03-22 20:45:39 -0300

    avadisplay for note

--- machines/gaming/.config/wm/xorg_local.sh
+++ machines/gaming/.config/wm/xorg_local.sh
@@ -3,6 +3,14 @@
 HOST_NAME="$HOSTNAME"
 CHOSE=$1
 
+ENV_DISPLAY="$AVA_DISPLAY"
+
+if [ -z "$AVA_DISPLAY" ]; then
+    echo "$AVA_DISPLAY"
+    exit 0
+fi
+
+
 if [ "$HOST_NAME" == "persistence" ]; then
     if [ "${CHOSE}" == "yes" ]; then
         echo "DisplayPort-2"
